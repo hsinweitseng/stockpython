d824a58ce99d2dcce55cf912a5e2301615c5befb
hsinweitseng <hsinwei.tseng@gmail.com>
2015-10-06 00:00:44 -0700

    test

---
+++ pandas02.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+
+web_site = {'Day':[1,2,3,4,5,6]}
+
+df = pd.DataFrame(web_site)
+
+print(df)
+
+
+
+df2 = pd.DataFrame(web_site)
+print(df2)
+df2.append = [1,2]
+print(df2)
+
+df.DataFrame.append(1)
+
+
